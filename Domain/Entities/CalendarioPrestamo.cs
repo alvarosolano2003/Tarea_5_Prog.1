9ef48fad09e32f0b9f879a97ebeb37f96002c774
MoisesNunez435 <moidavnused@gmail.com>
2021-11-24 11:23:48 -0600

    Agregando Domain

---
+++ Domain/Entities/CalendarioPrestamo.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CalendarioPrestamo
+    {
+        public int Id { get; set; }
+        public DateTime FechaPago { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public decimal Principal { get; set; }
+        public decimal PrincipalPagado { get; set; }
+        public decimal Interes { get; set; }
+        public decimal InteresPagado { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal CuotaPagado { get; set; }
+        public Estado Estado { get; set; }
+
+
+    }
+}
